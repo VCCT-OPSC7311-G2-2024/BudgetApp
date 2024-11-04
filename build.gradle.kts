7c73686965e39a5d5a732f61c213e3c505e733a2
Ben Cameron Rush <103778443+ST10132792@users.noreply.github.com>
2024-11-04 23:05:06 +0200

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    id("org.jetbrains.kotlin.jvm") version "2.0.21" apply false
-    alias(libs.plugins.kotlinCompose) apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
-}
